d60b00932de4faa5f191c12596f679581b301905
BestMat <76582849+BestMat@users.noreply.github.com>
2024-06-20 21:31:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// © 2024 - BestDeveloper - BestMat, Inc. - All rights reserved.
+const express = require("express");
+const app = express();
+module.exports.get = app.get;
+module.exports.listen = app.listen;
